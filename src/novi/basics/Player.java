c33a8b4a98e1cfc5c3bd492fcd9dea815bb5baf4
CTTY-NUL <66295799+CTTY-NUL@users.noreply.github.com>
2020-06-07 02:58:12 +0200

    eerste versiesadas

---
+++ src/novi/basics/Player.java
@@ -0,0 +1,46 @@
+package novi.basics;
+
+public class Player {
+    //attributen: informatie verzamelen
+    private String name;
+    private char token;
+    private int score;
+    private int wins;
+
+    //methoden: acties die de speler uit kan voeren
+    //constructor
+    public Player(String name, char token) {
+        this.name = name;
+        this.token = token;
+        score = 0;
+        wins = 0;
+    }
+
+    //get methoden
+    public String getName() {
+        return name;
+    }
+
+    public char getToken() {
+        return token;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    //set methoden
+    /*public void setScore(int score) {
+        this.score = score;
+    }*/
+    public void addScore() {
+        score++;
+    }
+    public void addWin() {
+        //parameter voor welke speler gewonnen heeft???
+    }
+}
